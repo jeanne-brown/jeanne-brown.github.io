3dc0b96829924c9595871f9c3f3a0bec60c04caa
jeanne-brown <156835064+jeanne-brown@users.noreply.github.com>
2025-07-06 14:41:39 +0000

    Deploying to gh-pages from @ jeanne-brown/jeanne-brown.github.io@156adb0117fafd9455c16aec89249a5d9832c4b1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,14 +501,35 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6A%65%61%6E%6E%65.%62%72%6F%77%6E@%6D%61%69%6C.%6D%63%67%69%6C%6C.%63%61", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/jeanne-brown", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/jeanne-brown-", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0009-0007-8280-780X", "_blank");
+        },
+      },{
+        id: 'social-osf',
+        title: 'Open Science Framework',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://osf.io/pkuzb/", "_blank");
         },
       },{
         id: 'social-rss',
@@ -516,13 +537,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
