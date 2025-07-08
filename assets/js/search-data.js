cdc2d04ba49b5702fe79a73ee28ef870bd3dad4c
jeanne-brown <156835064+jeanne-brown@users.noreply.github.com>
2025-07-08 05:32:34 +0000

    Deploying to gh-pages from @ jeanne-brown/jeanne-brown.github.io@94a38d0abff10459745770a70fc887d9258dc59c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-beyond-academia",
+    title: "beyond academia",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
