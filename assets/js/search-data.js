a1d7d4273da28f0a194e28f45be9ac41efb92078
jeanne-brown <156835064+jeanne-brown@users.noreply.github.com>
2025-07-09 22:51:40 +0000

    Deploying to gh-pages from @ jeanne-brown/jeanne-brown.github.io@c3fe180f4c3b7f9b3e4b594f34571015864962b4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -49,21 +49,6 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_enigma_of_reason/";
-            },},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
             },},{
         id: 'social-email',
         title: 'email',
