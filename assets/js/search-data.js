5b76f93da21f46649ab446964cf60d106644cac8
jeanne-brown <156835064+jeanne-brown@users.noreply.github.com>
2025-07-09 18:42:32 +0000

    Deploying to gh-pages from @ jeanne-brown/jeanne-brown.github.io@2f9fcc89917e551272cd962cf0c0a0aeaf745dcb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-blog",
-          title: "Blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
         },{id: "nav-teaching",
           title: "Teaching",
           description: "",
@@ -43,6 +36,13 @@ ninja.data = [{
           section: "Navigation",
           handler: () => {
             window.location.href = "/beyond/";
+          },
+        },{id: "nav-blog",
+          title: "Blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
           },
         },{id: "post-a-post-with-image-galleries",
         
