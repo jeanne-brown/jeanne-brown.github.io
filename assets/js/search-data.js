4b647b2004354e8aecf15aaefef27d67214ad1db
jeanne-brown <156835064+jeanne-brown@users.noreply.github.com>
2025-07-09 18:38:35 +0000

    Deploying to gh-pages from @ jeanne-brown/jeanne-brown.github.io@fc867b939901ab47d44ef3a244b8ae3e728c0e55 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-beyond-academia",
           title: "Beyond academia",
-          description: "",
+          description: "From data to downtime",
           section: "Navigation",
           handler: () => {
             window.location.href = "/beyond/";
