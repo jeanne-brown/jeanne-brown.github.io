91fb8625f37c99672553a07c3471f22b53e9b0d1
jeanne-brown <156835064+jeanne-brown@users.noreply.github.com>
2025-07-10 21:08:43 +0000

    Deploying to gh-pages from @ jeanne-brown/jeanne-brown.github.io@8dd1f6f0e0022440d8f6bf7495c3e83d0632f0a0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,12 +44,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/beyond/";
           },
-        },{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-i-am-pleased-to-announce-that-i-was-awarded-a-lsa-general-institute-fellowship-and-will-be-attending-the-lsa-summer-institute-july-august-2025",
+        },{id: "news-i-am-pleased-to-announce-that-i-was-awarded-a-lsa-general-institute-fellowship-and-will-be-attending-the-lsa-summer-institute-july-august-2025",
           title: 'I am pleased to announce that I was awarded a LSA General Institute...',
           description: "",
           section: "News",},{id: "news-i-was-awarded-an-frqsc-fonds-de-recherche-du-québec-société-et-culture-doctoral-grant-for-may-2025-to-september-2027-tada",
