fc474bd83e3e60c18c18e7b42315414d17c32d23
jeanne-brown <156835064+jeanne-brown@users.noreply.github.com>
2026-03-27 17:10:12 +0000

    Deploying to gh-pages from @ jeanne-brown/jeanne-brown.github.io@a60f9c14c90b9c7f69d9dce4cd8699756b270f2c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "Projects",
-          description: "A growing collection of your cool projects.",
+          description: "Research projects &amp; ongoing work",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
