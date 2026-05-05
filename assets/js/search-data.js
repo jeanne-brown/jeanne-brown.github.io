f9fce3677a5a096be806f5e334287bbba6907102
jeanne-brown <156835064+jeanne-brown@users.noreply.github.com>
2026-05-05 13:58:29 +0000

    Deploying to gh-pages from @ jeanne-brown/jeanne-brown.github.io@2b18a46306c4567822e0b349906281962350d16a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "Last updated 10 July 2025. Download PDF for full CV.",
+          description: "Last updated 25 March 2026. Download PDF for full CV.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
