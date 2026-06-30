2e4b21eaf91bb3e3de27c3331e3b7181d943b140
jeanne-brown <156835064+jeanne-brown@users.noreply.github.com>
2026-06-30 17:08:55 +0000

    Deploying to gh-pages from @ jeanne-brown/jeanne-brown.github.io@04e7e30c391a8cba41238d1eebd2afd93c17e145 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -51,7 +51,29 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/beyond/";
           },
-        },{id: "post-lfdi2024-in-shippagan",
+        },{id: "post-moth2025-in-toronto-and-cla-in-montreal",
+        
+          title: "moth2025 in Toronto and CLA in Montreal!",
+        
+        description: "Attending moth2025 and CLA2025 to present my creak mediation work and a bit of sight-seeing in Toronto",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/mothCLA2025/";
+          
+        },
+      },{id: "post-labphon19-in-seoul",
+        
+          title: "LabPhon19 in Seoul!",
+        
+        description: "Attending LabPhon19 to present my work and doing tourist things in Seoul",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2024/LabPhon19/";
+          
+        },
+      },{id: "post-lfdi2024-in-shippagan",
         
           title: "LFDI2024 in Shippagan!",
         
