b732597f722d935121dfe9970038baa007f62b99
jeanne-brown <156835064+jeanne-brown@users.noreply.github.com>
2026-06-30 17:09:34 +0000

    Deploying to gh-pages from @ jeanne-brown/jeanne-brown.github.io@83a7cc060dc7f9281e6d656ea8ff7a3a40d1334e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -51,18 +51,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/beyond/";
           },
-        },{id: "post-labphon-20-in-montreal",
-        
-          title: "LabPhon 20 in Montreal",
-        
-        description: "LabPhon20 --  a recap of organizing a milestone conference in Montreal, complete with snack tables, a poster, and a well-earned rest after",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2026/LabPhon20/";
-          
-        },
-      },{id: "post-asa-in-philly",
+        },{id: "post-asa-in-philly",
         
           title: "ASA in Philly",
         
