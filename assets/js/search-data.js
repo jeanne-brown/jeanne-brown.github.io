e1cdcd925bbaf66317b5ead738b820644262e267
jeanne-brown <156835064+jeanne-brown@users.noreply.github.com>
2026-06-30 17:09:12 +0000

    Deploying to gh-pages from @ jeanne-brown/jeanne-brown.github.io@f55fc682dc9424cd17bd716618d94901a8790237 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -51,7 +51,29 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/beyond/";
           },
-        },{id: "post-moth2025-in-toronto-and-cla-in-montreal",
+        },{id: "post-asa-asj-in-honolulu",
+        
+          title: "ASA/ASJ in Honolulu",
+        
+        description: "Attending ASA/ASJ 2025 to present my work and enjoying Honolulu",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/ASA2025/";
+          
+        },
+      },{id: "post-lsa-summer-institute-2025",
+        
+          title: "LSA Summer Institute 2025",
+        
+        description: "5 weeks of linguistics summer school in Eugene, Oregon",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/LSAsummer2025/";
+          
+        },
+      },{id: "post-moth2025-in-toronto-and-cla-in-montreal",
         
           title: "moth2025 in Toronto and CLA in Montreal!",
         
